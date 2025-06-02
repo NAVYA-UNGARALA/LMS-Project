24915ced5952b6cb0f11542d788dab0c66b15ecc
NAVYA-UNGARALA <153704368+NAVYA-UNGARALA@users.noreply.github.com>
2025-06-02 13:01:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,9 +17,12 @@ app.use("/api/v1/purchase/webhook", express.raw({ type: "application/json" }));
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+    origin: [
+        "http://localhost:5173",
+        "https://lms-project-n1ir.onrender.com"
+    ],
+    credentials: true
+}));
 app.use((req, res, next) => {
     next()
 })
