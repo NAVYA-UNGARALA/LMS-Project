9d6314e2d43ab122ed05b5ac957e4ddbb621ffda
NAVYA-UNGARALA <153704368+NAVYA-UNGARALA@users.noreply.github.com>
2025-06-02 13:26:28 +0530

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,7 +8,8 @@ export const generateToken = (res, user, message) => {
     .status(200)
     .cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none", // <-- allow cross-site cookies
+      secure: true,     // <-- required for sameSite: "none"
       maxAge: 24 * 60 * 60 * 1000,
     })
     .json({
